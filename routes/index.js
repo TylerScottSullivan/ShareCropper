3014579d6b0f962e6299e728272a819c6c758e41
AndyMartin93 <amarti40@villanova.edu>
2016-07-20 18:05:25 -0400

    format

--- routes/index.js
+++ routes/index.js
@@ -111,59 +111,60 @@ router.post('/creategarden', function(req, res, next) {
 router.get('/uploadphoto/:cid', function(req,res,next){
 	res.render('uploadphoto', {cid: req.params.cid});
 });
+
 router.post('/uploadphoto/:cid', upload.single('file'), function(req,res,next){
 	//Making S3 work
-var client = s3.createClient({
-  maxAsyncS3: 20,     // this is the default
-  s3RetryCount: 3,    // this is the default
-  s3RetryDelay: 1000, // this is the default
-  multipartUploadThreshold: 20971520, // this is the default (20 MB)
-  multipartUploadSize: 15728640, // this is the default (15 MB)
-  s3Options: {
-    accessKeyId: process.env.accessKeyId,
-    secretAccessKey: process.env.secretAccessKey,
-    region: "us-east-1",
-    signatureVersion:"v3",
-    
-    // endpoint: 's3.yourdomain.com',
-    // sslEnabled: false
-    // any other options are passed to new AWS.S3()
-    // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Config.html#constructor-property
-  },
-});
+	var client = s3.createClient({
+	  maxAsyncS3: 20,     // this is the default
+	  s3RetryCount: 3,    // this is the default
+	  s3RetryDelay: 1000, // this is the default
+	  multipartUploadThreshold: 20971520, // this is the default (20 MB)
+	  multipartUploadSize: 15728640, // this is the default (15 MB)
+	  s3Options: {
+	    accessKeyId: process.env.accessKeyId,
+	    secretAccessKey: process.env.secretAccessKey,
+	    region: "us-east-1",
+	    signatureVersion:"v3",
+	    
+	    // endpoint: 's3.yourdomain.com',
+	    // sslEnabled: false
+	    // any other options are passed to new AWS.S3()
+	    // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Config.html#constructor-property
+	  },
+	});
 
-var params = {
-  localFile: req.file.path,
-  s3Params: {
-    Bucket: "sharecropper",
-    Key: "some/remote/"+req.user.cropArr[0]+".png",
-    ACL: "bucket-owner-full-control",
-    ContentType: "image/png"
-    // other options supported by putObject, except Body and ContentLength.
-    // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#putObject-property
-  },
-};
-var uploader = client.uploadFile(params);
-uploader.on('error', function(err) {
-  console.error("unable to upload:", err.stack);
-});
-uploader.on('progress', function() {
-  console.log("progress", uploader.progressMd5Amount,
-            uploader.progressAmount, uploader.progressTotal);
-});
-uploader.on('end', function() {
-	var image_URI = 'https://s3-us-west-2.amazonaws.com/sharecropper/some/remote/'+req.params.cid+'.png';
-		models.Crop.findByIdAndUpdate(req.params.cid, {
-			'image': image_URI
-				},function(err, crop){
-					if(err){
-						console.log(err);
-					}
-					res.redirect('/myprofile')
+	var params = {
+	  localFile: req.file.path,
+	  s3Params: {
+	    Bucket: "sharecropper",
+	    Key: "some/remote/"+req.user.cropArr[0]+".png",
+	    ACL: "bucket-owner-full-control",
+	    ContentType: "image/png"
+	    // other options supported by putObject, except Body and ContentLength.
+	    // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#putObject-property
+	  },
+	};
+	var uploader = client.uploadFile(params);
+	uploader.on('error', function(err) {
+	  console.error("unable to upload:", err.stack);
+	});
+	uploader.on('progress', function() {
+	  console.log("progress", uploader.progressMd5Amount,
+	            uploader.progressAmount, uploader.progressTotal);
+	});
+	uploader.on('end', function() {
+		var image_URI = 'https://s3-us-west-2.amazonaws.com/sharecropper/some/remote/'+req.params.cid+'.png';
+			models.Crop.findByIdAndUpdate(req.params.cid, {
+				'image': image_URI
+					},function(err, crop){
+						if(err){
+							console.log(err);
+						}
+						res.redirect('/myprofile')
 
-		});
-  console.log("done uploading");
-});
+			});
+	  console.log("done uploading");
+	});
 });
 
 router.post('/creategarden', function(req, res, next) {
